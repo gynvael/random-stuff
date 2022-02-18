39831995b719c33cc3ab6bcb43eede8156deeb62
Gynavel Coldwind <gynvael@coldwind.pl>
2022-02-18 09:54:14 +0100

    Fixed some bugs in TTS, added PL

--- text-to-speech/tts_worker.py
+++ text-to-speech/tts_worker.py
@@ -9,13 +9,14 @@ import sys
 import os
 
 def show_usage_and_exit():
-  sys.exit("usage: tts.py [-de|-en] <textfile>")
+  sys.exit("usage: tts.py [-de|-en|-pl] <textfile>")
 
 if len(sys.argv) not in {2, 3}:
   show_usage_and_exit()
 
 VOICE_NAME = "en-US-Wavenet-F"
 SPEAKING_RATE = 1.25
+VOICE_PITCH = 0
 
 if len(sys.argv) == 3:
   if sys.argv[1] == "-de":
@@ -23,6 +24,10 @@ if len(sys.argv) == 3:
     SPEAKING_RATE = 1.0
   elif sys.argv[1] == "-en":
     pass  # Default.
+  elif sys.argv[1] == "-pl":
+    VOICE_NAME = "pl-PL-Wavenet-B"
+    VOICE_PITCH = -8
+    SPEAKING_RATE = 1.5
   else:
     show_usage_and_exit()
 
@@ -45,7 +50,8 @@ def text_to_mp3(fname, text):
 
   audio_config = texttospeech.AudioConfig(
       audio_encoding=texttospeech.AudioEncoding.MP3,
-      speaking_rate=SPEAKING_RATE
+      speaking_rate=SPEAKING_RATE,
+      pitch=VOICE_PITCH
   )
 
   # TODO: Perhaps it's enough to keep a reference to the client?
@@ -70,26 +76,31 @@ text = iter(text.splitlines())
 f = open("parts.txt", "w")
 
 part = 1
-while True:
+final = False
+while not final:
   t = ""
-  final = False
-  while len(t) < 5000:
+  while len(t) < 2500:
     prev_t = t
-    t += "\n"
     try:
-      t += next(text)
+      nt = next(text).strip()
+      if not nt:
+        continue
+      t += nt + ".\n"
     except StopIteration:
       final = True
       break
 
-  if len(prev_t.strip()) == 0:
-    break
+  #if len(prev_t.strip()) == 0:
+  #  break
 
   if len(prev_t) == 0 and not final:
     sys.exit("handle this case where the line is more than 5k...")
 
   fname = "part_%.3i.mp3" % part
   f.write(f"file '{fname}'\n")
+
+  with open(f"{fname}.txt", "w", encoding="utf-8") as g:
+    g.write(t)
 
   text_to_mp3(fname, prev_t)
   part += 1
